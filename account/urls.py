be60ebabf9fefe1b156e384976223d1a47c87c82
sanskarkakde14 <sanskarkakde13@gmail.com>
2024-06-19 16:07:30 +0530

    pycache removal

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from account.views import SendPasswordResetEmailView, UserChangePasswordView, UserLoginView, UserProfileView, UserRegistrationView, UserPasswordResetView
+from rest_framework_simplejwt import views as jwt_views
+
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
@@ -7,5 +9,6 @@ urlpatterns = [
     path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
     path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
     path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),
-
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name ='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token_refresh')
 ]
